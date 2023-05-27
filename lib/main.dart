afc91443193e8f4af5f385060e57804c0f570a82
Italo Matos <attackdiamond@gmail.com>
2023-05-26 23:55:34 -0300

    CHANGE: movido os arquivos do projeto para SRC

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:whatsapp_messenger/core/main_app/main_app.dart';
-import 'package:whatsapp_messenger/core/main_app/main_module.dart';
-
+import 'package:whatsapp_messenger/src/core/main_app/main_app.dart';
+import 'package:whatsapp_messenger/src/core/main_app/main_module.dart';
 void main(List<String> args) {
   runApp(ModularApp(module: MainModule(), child: const MainApp()));
 }
---
+++ lib/src/modules/wellcome_page/views/wellcome_view.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class WellcomeView extends StatelessWidget {
+  const WellcomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
